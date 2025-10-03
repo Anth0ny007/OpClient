e5be95e4d28e3f0a037429c791d44bfd3a1c1086
Anthony Urbaez <20231394@itla.edu.do>
2025-10-03 16:48:12 -0400

    Terminos proyecto

--- Inicio.cs
+++ Inicio.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.Configuration;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -12,25 +12,29 @@ class Program
         Console.WriteLine("=== INICIO ETL ===");
 
         var extractor = new ExtractService();
+        var transformar = new TransformService();
+        var carga = new LoadService(connStr);
 
         // 1. Cargar Clientes
         var clientes = extractor.LeerClientes("ArchivoCsv/clients.csv");
-        var loader = new LoadService(connStr);
-        loader.InsertarClientes(clientes);
+        carga.InsertarClientes(clientes);
+        Console.WriteLine($"Clientes procesados: {clientes.Count}");
 
         // 2. Cargar Productos
         var productos = extractor.LeerProductos("ArchivoCsv/products.csv");
-        loader.InsertarProductos(productos);
+        carga.InsertarProductos(productos);
+        Console.WriteLine($"Productos procesados: {productos.Count}");
 
         // 3. Cargar Fuentes
         var fuentes = extractor.LeerFuentes("ArchivoCsv/fuente_datos.csv");
-        loader.InsertarFuentes(fuentes);
+        carga.InsertarFuentes(fuentes);
+        Console.WriteLine($"Fuentes procesadas: {fuentes.Count}");
 
         // 4. Cargar Opiniones
         var opinionesRaw = extractor.LeerOpiniones("ArchivoCsv/surveys_part1.csv");
-        var transformar = new TransformService();
         var opinionesLimpias = transformar.LimpiarYClasificar(opinionesRaw);
-        loader.InsertarDatos(opinionesLimpias);
+        carga.InsertarDatos(opinionesLimpias);
+        Console.WriteLine($"Opiniones cargadas: {opinionesLimpias.Count}");
 
         Console.WriteLine("=== ETL COMPLETADO ===");
     }
